9fa5f5e7dc801f7f0e44590a8ca9783e968c90a6
Guilherme Lima <guigui410@gmail.com>
2020-06-10 16:34:21 -0300

    trabalhando no controller Categoria

---
+++ LojaVirtual/Areas/Colaborador/Controllers/CategoriaController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LojaVirtual.Libraries.Filtro;
+using LojaVirtual.Models;
+using LojaVirtual.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtual.Areas.Colaborador.Controllers
+{
+    [Area("Colaborador")]
+    //[ColaboradorAutorizacao]
+    public class CategoriaController : Controller
+    {
+        private ICategoriaRepository categoriaRepository;
+
+        public CategoriaController(ICategoriaRepository _categoriaRepository)
+        {
+            categoriaRepository = _categoriaRepository;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Categoria> ListaCategorias = categoriaRepository.TodasCategorias();
+            return View(ListaCategorias);
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Cadastrar([FromForm]Categoria categoria)
+        {
+            //TODO Implementar Logica
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Atualizar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Atualizar([FromForm]Categoria categoria)
+        {
+            //TODO Implementar Logica
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Excluir(int id)
+        {
+            return View();
+        }
+
+    }
+}
