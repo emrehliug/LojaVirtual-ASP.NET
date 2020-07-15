486d03e80aa3059ea06106fda58986e5b32ac13b
unknown <guigui410@gmail.com>
2020-07-15 13:40:37 -0300

    criado esqueledo de crud para colaborador, adaptando conexão para minha nova instancia SQL

--- LojaVirtual/Models/Colaborador.cs
+++ LojaVirtual/Models/Colaborador.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using LojaVirtual.Libraries.Lang;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,14 +13,24 @@ namespace LojaVirtual.Models
         [Display(Name = "Código")]
         public int Id { get; set; }
 
-        [Display(Name = "Código")]
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
+        [MinLength(4, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E002")]
         public string Nome { get; set; }
+
+        [Display(Name = "E-Mail")]
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
+        [EmailAddress(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E004")]
         public string Email { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
+        [MinLength(6, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E002")]
         public string Senha { get; set; }
 
-
+        [NotMapped]
         public string ConfirmaSenha { get; set; }
         //prop tipo tem como objetivo definir C = Comum / G = Gerente 
+
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
         public string Tipo { get; set; }
     }
 }
